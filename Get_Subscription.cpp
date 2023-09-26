e18428f88f71804c698b91fa1de09eae96b74a3b
Adi-Raj007 <134602591+Adi-Raj007@users.noreply.github.com>
2023-09-26 17:38:44 +0530

    Get_Subscription.cpp

---
+++ Get_Subscription.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void testcase(){
+    int X,Y;
+    cin>>X;
+    if(X>30)
+    cout<<"YES"<<endl;
+    else 
+    cout<< "NO"<<endl;
+}
+
+
+
+int main() {
+	
+	int tt;
+	cin >> tt;
+	while(tt--){
+	    testcase();
+	}
+	return 0;
+}
