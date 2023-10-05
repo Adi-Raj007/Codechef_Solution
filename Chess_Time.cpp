0a760bce67327905e535e44324450a8438d4a336
Adi-Raj007 <134602591+Adi-Raj007@users.noreply.github.com>
2023-10-05 13:34:40 +0530

    Create Chess_Time.cpp

---
+++ Chess_Time.cpp
@@ -0,0 +1,26 @@
+/* Chef has recently started playing chess, and wants to play as many games as possible.
+He calculated that playing one game of chess takes at least 20 minutes of his time. Chef has N hours of free time. What is the maximum number of complete chess games he can play in that time?
+*/
+
+#include <iostream>
+using namespace std;
+
+void testcase(){int X;
+cin>>X;
+int Y=(X*60/20);
+cout<<Y<<endl;
+    
+    
+}
+
+
+
+int main() {
+	
+	int tt;
+	cin >> tt;
+	while(tt--){
+	    testcase();
+	}
+	return 0;
+}
