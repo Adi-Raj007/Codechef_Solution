76ca55faab3fb4a1e43ff36dcf745769bd55c81f
Adi-Raj007 <134602591+Adi-Raj007@users.noreply.github.com>
2023-10-13 23:27:51 +0530

    Create podiu_finish.cpp

---
+++ podiu_finish.cpp
@@ -0,0 +1,23 @@
+/*Chef got his dream seat in F1 and secured a 3 rd place in his debut race. He now wants to know the time gap between him and the winner of the race.
+You are his chief engineer and you only know the time gap between Chef and the runner up of the race, given as A seconds, and the time gap between the runner up and the winner of the race,
+given as B seconds.Please calculate and inform Chef about the time gap between him and the winner of the race.*/
+#include <iostream>
+using namespace std;
+
+void testcase(){int X,Y;
+cin>>X>>Y;
+cout<<X+Y<<endl;
+    
+}
+
+
+
+int main() {
+	
+	int tt;
+	cin >> tt;
+	while(tt--){
+	    testcase();
+	}
+	return 0;
+}
